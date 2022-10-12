63dd6b120f45d91f5a31fd0b6f838956c4507c2c
407 <407@academicos.cesde.edu.co>
2022-10-12 17:26:42 -0500

    actual

---
+++ Restaurante3/app/src/main/java/com/example/restaurante3/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.restaurante3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance(); // instancia de Firestore
+    String idCustomer; // variable que contendra el id de cada cliente (customer)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Instanciar y referenciar los ID's del archivo xml
+        EditText ident = findViewById(R.id.etident);
+        EditText fullname = findViewById(R.id.etfullname);
+        EditText email = findViewById(R.id.etemail);
+        EditText password = findViewById(R.id.etpassword);
+        Button btnsave = findViewById(R.id.save);
+        Button btnsearch = findViewById(R.id.search);
+        Button btnedit = findViewById(R.id.edit);
+        Button btndelete = findViewById(R.id.delete);
+
+        btnsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("customer")
+                        .whereEqualTo("ident", ident.getText().toString())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (!task.getResult().isEmpty()) {// si entrontro el documento
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            idCustomer = document.getId();
+                                            fullname.setText(document.getString("fullname"));
+                                            email.setText(document.getString("email"));
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),"El id del cliente No existe ",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveCustomer(ident.getText().toString(),fullname.getText().toString(), email.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+
+
+
+    private void saveCustomer(String sident, String sfullnane, String semail, String spassword) {
+       // Buscar la identificacion del cliente nuevo
+        db.collection("customer")
+                .whereEqualTo("ident", sident)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().isEmpty()) {// No entrontro el documento, se puede guardar
+                               // Guardar los datos del cliente (customer)
+                                Map<String, Object> customer = new HashMap<>(); // Tabla cursor
+                                customer.put("ident", sident);
+                                customer.put("fullname", sfullnane);
+                                customer.put("email", semail);
+                                customer.put("password", spassword);
+
+                                db.collection("customer")
+                                        .add(customer)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                //Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                                Toast.makeText(getApplicationContext(), "Cliente agregado correctamente...",Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(getApplicationContext(), "Error! Cliente no se guardó...",Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"El id del cliente existente... ",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+
+
+    }
+}
